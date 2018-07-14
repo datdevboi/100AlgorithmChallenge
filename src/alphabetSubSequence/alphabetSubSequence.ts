17914f16256d307b5f173b9677cb3498b470d4f6
camilo17 <cami172013@gmail.com>
2018-07-14 18:20:28 -0500

    solved alphabetSubsequence

--- src/alphabetSubSequence/alphabetSubSequence.ts
+++ src/alphabetSubSequence/alphabetSubSequence.ts
@@ -1,9 +1,46 @@
 function alphabetSubsequence(s: string): boolean {
+  const alphabet: Record<string, number> = {
+    a: 1,
+    b: 2,
+    c: 3,
+    d: 4,
+    e: 5,
+    f: 6,
+    g: 7,
+    h: 8,
+    i: 9,
+    j: 10,
+    k: 11,
+    l: 12,
+    m: 13,
+    n: 14,
+    o: 15,
+    p: 16,
+    q: 17,
+    r: 18,
+    s: 19,
+    t: 20,
+    u: 21,
+    v: 22,
+    w: 23,
+    x: 24,
+    y: 25,
+    z: 26
+  };
 
+  const strArr = s.split("");
+
+  for (let i = 0; i < strArr.length - 1; i++) {
+    if (alphabet[strArr[i]] >= alphabet[strArr[i + 1]]) {
+      return false;
+    }
+  }
+
+  return true;
 }
 
-console.log(alphabetSubsequence('zab'))
-console.log(alphabetSubsequence('effg'))
-console.log(alphabetSubsequence('cdce'))
-console.log(alphabetSubsequence('ace'))
-console.log(alphabetSubsequence('bxz'))
+console.log(alphabetSubsequence("zab"));
+console.log(alphabetSubsequence("effg"));
+console.log(alphabetSubsequence("cdce"));
+console.log(alphabetSubsequence("ace"));
+console.log(alphabetSubsequence("bxz"));
