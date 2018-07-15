de85ec1c2b758064a39f14ce05f98e66a42f2537
camilo17 <cami172013@gmail.com>
2018-07-15 18:28:37 -0500

    solved areEquallyStrong

--- src/areEquallyStrong/areEquallyStrong.ts
+++ src/areEquallyStrong/areEquallyStrong.ts
@@ -1,5 +1,17 @@
-function areEquallyStrong(yourLeft: number, yourRight: number, friendsLeft: number, friendsRight: number): boolean {
+function areEquallyStrong(
+  yourLeft: number,
+  yourRight: number,
+  friendsLeft: number,
+  friendsRight: number
+): boolean {
+  const yourWeakest = yourLeft <= yourRight ? yourLeft : yourRight;
+  const yourStrongest = yourLeft <= yourRight ? yourRight : yourLeft;
+  const friendsWeakest =
+    friendsLeft <= friendsRight ? friendsLeft : friendsRight;
+  const friendsStrongest =
+    friendsLeft <= friendsRight ? friendsRight : friendsLeft;
 
+  return yourStrongest === friendsStrongest && yourWeakest === friendsWeakest;
 }
 
 // console.log(areEquallyStrong(10, 15, 15, 10))
